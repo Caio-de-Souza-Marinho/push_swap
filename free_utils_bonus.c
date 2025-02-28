9dec98abdeb09be27c1b734cbe81d0adf3fbac1b
Caio De Souza Marinho <caide-so@c1r4p2.42sp.org.br>
2025-02-27 23:00:56 -0300

    rename files

--- free_utils_bonus.c
+++ free_utils_bonus.c
@@ -1,48 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   1_sort_stack_bonus.c                               :+:      :+:    :+:   */
+/*   free_utils_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/26 21:37:25 by caide-so          #+#    #+#             */
-/*   Updated: 2025/02/26 21:37:59 by caide-so         ###   ########.fr       */
+/*   Created: 2025/02/26 20:00:11 by caide-so          #+#    #+#             */
+/*   Updated: 2025/02/27 23:00:30 by caide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-int	is_sorted(t_stack *stack)
-{
-	t_stack	*curr;
-
-	curr = stack;
-	while (curr->next)
-	{
-		if (curr->value > curr->next->value)
-			return (0);
-		curr = curr->next;
-	}
-	return (1);
-}
-
-void	current_index(t_stack *stack)
+// free a array of strings allocated with the ft_split function
+void	free_split(char **split)
 {
 	int	i;
-	int	median;
 
 	i = 0;
-	if (!stack)
-		return ;
-	median = stack_len(stack) / 2;
-	while (stack)
+	while (split[i])
 	{
-		stack->index = i;
-		if (i <= median)
-			stack->above_median = 1;
-		else
-			stack->above_median = 0;
-		stack = stack->next;
+		free(split[i]);
 		i++;
 	}
+	free(split);
+}
+
+void	cleanup(char *str, long *nbrs)
+{
+	if (str)
+		free(str);
+	if (nbrs)
+		free(nbrs);
+}
+
+void	free_stacks(t_stack **stack_a, t_stack **stack_b)
+{
+	free_stack(stack_a);
+	free_stack(stack_b);
 }
